050cae3de8190539dca8609c26bd24fb974e5d0d
joshuabalanza <joshuabalanza@gmail.com>
2018-07-28 15:21:51 +0800

    Login2

--- COLAS/Login.Designer.cs
+++ COLAS/Login.Designer.cs
@@ -28,21 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            BunifuAnimatorNS.Animation animation2 = new BunifuAnimatorNS.Animation();
             this.pnLogin1 = new System.Windows.Forms.Panel();
             this.lblPassword = new System.Windows.Forms.Label();
+            this.txtbxPassword = new System.Windows.Forms.TextBox();
             this.lblUsername = new System.Windows.Forms.Label();
-            this.txtbxUsername = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.txtbxUsername = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnCancel = new MaterialSkin.Controls.MaterialRaisedButton();
             this.btnLogin = new MaterialSkin.Controls.MaterialRaisedButton();
-            this.txtbxPassword = new Bunifu.Framework.UI.BunifuMetroTextbox();
             this.pnLogin2 = new System.Windows.Forms.Panel();
             this.lblLogin2 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.bunifuTransition1 = new BunifuAnimatorNS.BunifuTransition(this.components);
             this.pnLogin1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.pnLogin2.SuspendLayout();
@@ -55,13 +52,12 @@
             this.pnLogin1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnLogin1.BackgroundImage")));
             this.pnLogin1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pnLogin1.Controls.Add(this.lblPassword);
+            this.pnLogin1.Controls.Add(this.txtbxPassword);
             this.pnLogin1.Controls.Add(this.lblUsername);
             this.pnLogin1.Controls.Add(this.txtbxUsername);
             this.pnLogin1.Controls.Add(this.pictureBox1);
             this.pnLogin1.Controls.Add(this.btnCancel);
             this.pnLogin1.Controls.Add(this.btnLogin);
-            this.pnLogin1.Controls.Add(this.txtbxPassword);
-            this.bunifuTransition1.SetDecoration(this.pnLogin1, BunifuAnimatorNS.DecorationType.None);
             this.pnLogin1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnLogin1.Location = new System.Drawing.Point(0, 0);
             this.pnLogin1.Name = "pnLogin1";
@@ -74,12 +70,22 @@
             this.lblPassword.BackColor = System.Drawing.Color.Transparent;
             this.lblPassword.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(118)))));
-            this.lblPassword.Location = new System.Drawing.Point(510, 452);
+            this.lblPassword.Location = new System.Drawing.Point(510, 435);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(95, 22);
             this.lblPassword.TabIndex = 6;
             this.lblPassword.Text = "Password";
             this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtbxPassword
+            // 
+            this.txtbxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(10)))), ((int)(((byte)(22)))));
+            this.txtbxPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtbxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxPassword.Location = new System.Drawing.Point(498, 429);
+            this.txtbxPassword.Name = "txtbxPassword";
+            this.txtbxPassword.Size = new System.Drawing.Size(370, 35);
+            this.txtbxPassword.TabIndex = 8;
             // 
             // lblUsername
             // 
@@ -87,7 +93,7 @@
             this.lblUsername.BackColor = System.Drawing.Color.Transparent;
             this.lblUsername.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(118)))));
-            this.lblUsername.Location = new System.Drawing.Point(510, 373);
+            this.lblUsername.Location = new System.Drawing.Point(510, 376);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(101, 22);
             this.lblUsername.TabIndex = 5;
@@ -97,21 +103,12 @@
             // txtbxUsername
             // 
             this.txtbxUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(10)))), ((int)(((byte)(22)))));
-            this.txtbxUsername.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(118)))));
-            this.txtbxUsername.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(118)))));
-            this.txtbxUsername.BorderColorMouseHover = System.Drawing.Color.Blue;
-            this.txtbxUsername.BorderThickness = 3;
-            this.txtbxUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtbxUsername.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtbxUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(118)))));
-            this.txtbxUsername.isPassword = false;
-            this.txtbxUsername.Location = new System.Drawing.Point(498, 362);
-            this.txtbxUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbxUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtbxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxUsername.Location = new System.Drawing.Point(498, 370);
             this.txtbxUsername.Name = "txtbxUsername";
-            this.txtbxUsername.Size = new System.Drawing.Size(370, 44);
-            this.txtbxUsername.TabIndex = 0;
-            this.txtbxUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.txtbxUsername.OnValueChanged += new System.EventHandler(this.txtbxUsername_OnValueChanged);
+            this.txtbxUsername.Size = new System.Drawing.Size(370, 35);
+            this.txtbxUsername.TabIndex = 7;
             // 
             // pictureBox1
             // 
@@ -156,25 +153,6 @@
             this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
-            // txtbxPassword
-            // 
-            this.txtbxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(10)))), ((int)(((byte)(22)))));
-            this.txtbxPassword.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(118)))));
-            this.txtbxPassword.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(118)))));
-            this.txtbxPassword.BorderColorMouseHover = System.Drawing.Color.Blue;
-            this.txtbxPassword.BorderThickness = 3;
-            this.txtbxPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtbxPassword.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtbxPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(118)))));
-            this.txtbxPassword.isPassword = true;
-            this.txtbxPassword.Location = new System.Drawing.Point(498, 441);
-            this.txtbxPassword.Margin = new System.Windows.Forms.Padding(4);
-            this.txtbxPassword.Name = "txtbxPassword";
-            this.txtbxPassword.Size = new System.Drawing.Size(370, 44);
-            this.txtbxPassword.TabIndex = 1;
-            this.txtbxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.txtbxPassword.OnValueChanged += new System.EventHandler(this.txtbxPassword_OnValueChanged);
-            // 
             // pnLogin2
             // 
             this.pnLogin2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(10)))), ((int)(((byte)(22)))));
@@ -214,35 +192,13 @@
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
-            // bunifuTransition1
-            // 
-            this.bunifuTransition1.AnimationType = BunifuAnimatorNS.AnimationType.VertSlide;
-            this.bunifuTransition1.Cursor = null;
-            animation2.AnimateOnlyDifferences = true;
-            animation2.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.BlindCoeff")));
-            animation2.LeafCoeff = 0F;
-            animation2.MaxTime = 1F;
-            animation2.MinTime = 0F;
-            animation2.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.MosaicCoeff")));
-            animation2.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation2.MosaicShift")));
-            animation2.MosaicSize = 0;
-            animation2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 0);
-            animation2.RotateCoeff = 0F;
-            animation2.RotateLimit = 0F;
-            animation2.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.ScaleCoeff")));
-            animation2.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.SlideCoeff")));
-            animation2.TimeCoeff = 0F;
-            animation2.TransparencyCoeff = 0F;
-            this.bunifuTransition1.DefaultAnimation = animation2;
-            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1366, 768);
-            this.Controls.Add(this.pnLogin2);
             this.Controls.Add(this.pnLogin1);
-            this.bunifuTransition1.SetDecoration(this, BunifuAnimatorNS.DecorationType.None);
+            this.Controls.Add(this.pnLogin2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -261,17 +217,16 @@
         #endregion
 
         private System.Windows.Forms.Panel pnLogin1;
-        private Bunifu.Framework.UI.BunifuMetroTextbox txtbxUsername;
-        private Bunifu.Framework.UI.BunifuMetroTextbox txtbxPassword;
-        private MaterialSkin.Controls.MaterialRaisedButton btnCancel;
-        private MaterialSkin.Controls.MaterialRaisedButton btnLogin;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label lblPassword;
-        private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.Panel pnLogin2;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label lblLogin2;
-        private BunifuAnimatorNS.BunifuTransition bunifuTransition1;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private MaterialSkin.Controls.MaterialRaisedButton btnCancel;
+        private MaterialSkin.Controls.MaterialRaisedButton btnLogin;
+        private System.Windows.Forms.TextBox txtbxUsername;
+        private System.Windows.Forms.TextBox txtbxPassword;
     }
 }
 
